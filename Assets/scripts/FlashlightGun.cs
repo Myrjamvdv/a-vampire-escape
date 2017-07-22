3a821096e942ddd87d320eb2b5b2921b5a0941f4
Stijn Seghers <stijnseghers@gmail.com>
2017-07-22 14:12:59 +1200

    Flashlight controls

---
+++ Assets/scripts/FlashlightGun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightGun : MonoBehaviour
+{
+    private const float INTENSITY_MULTIPLIER = 10f;
+
+    public GameObject flashlight;
+
+    void FixedUpdate ()
+    {
+        HandleInput ();
+    }
+
+    void HandleInput ()
+    {
+        // Gather data
+        var basePosition = transform.position;
+        var mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        var mousePostionRelativeToBase = mousePosition - basePosition;
+        var mouseBaseDistance = Vector2.Distance (mousePosition, transform.position);
+        var mouseBaseAngle = Mathf.Atan2 (mousePostionRelativeToBase.y, mousePostionRelativeToBase.x);
+
+        // Set flashlight angle
+        flashlight.transform.rotation = Quaternion.Euler (-mouseBaseAngle * 180 / Mathf.PI, 90, 0);
+
+        // Set flashlight intensity
+        flashlight.GetComponent<Light> ().intensity = INTENSITY_MULTIPLIER * mouseBaseDistance;
+    }
+}

f2cda450ac36f3ececfa1fc01363df11712056d0
Stijn Seghers <stijnseghers@gmail.com>
2017-07-23 17:35:45 +1200

    Wired up intro scene

---
+++ Assets/scripts/GameStarter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour
+{
+    public string level1SceneName;
+
+    void Update ()
+    {
+        if (Input.GetMouseButtonUp (0)) {
+            SceneManager.LoadScene (level1SceneName);
+        }
+    }
+}

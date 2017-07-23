58426d87ba81e8ab45e5cf633ad6182fb61bc4dd
Stijn Seghers <stijnseghers@gmail.com>
2017-07-23 13:20:34 +1200

    Passthrough garlic

---
+++ Assets/scripts/LayerCollidesWith.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerCollidesWith : MonoBehaviour
+{
+    public string layerName;
+    public LayerMask layersToCollideWith;
+
+    void Start ()
+    {
+        var layer = LayerMask.NameToLayer (layerName);
+        Physics2D.SetLayerCollisionMask (layer, layersToCollideWith);
+    }
+}
